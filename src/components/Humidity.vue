7e7b0abcfb4b9dec720c6eb298aeb9e557eb2bbe
OdPavel <98313210+OdPavel@users.noreply.github.com>
2023-06-25 16:52:28 +0300

    first commit

---
+++ src/components/Humidity.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+  humidity:{
+    type: Number,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+  <section class="section-bottom">
+    <div
+        class="block-bottom"
+    >
+      <div class="block-bottom-inner">
+        <div class="block-bottom-pic pic-humidity"></div>
+        <div class="block-bottom-texts">
+          <div class="block-bottom-text-block">
+            <div class="block-bottom-text-block-title">
+              Влажность: {{ humidity }} %
+            </div>
+            <div class="block-bottom-text-block-desc">
+              Влажность – это концентрация водяного пара, присутствующего в воздухе. Водяной пар, газообразное состояние воды, как правило, невидим для человеческого глаза.
+              <br /><br />
+              Одно и то же количество водяного пара приводит к более высокой относительной влажности в холодном воздухе, чем в теплом.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/constans/index.js
@@ -0,0 +1,2 @@
+export const API_KEY = '971858dd590f0d8560e8e04d5c6ab59c'
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
